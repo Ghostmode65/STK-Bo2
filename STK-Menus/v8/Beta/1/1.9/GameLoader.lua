127dd97fdd1c9aae22f2bb0a120116a88f7de001
Ghostmode65 <gemcaster0@gmail.com>
2023-03-02 16:08:29 -0600

    Final Beta

---
+++ STK-Menus/v8/Beta/1/1.9/GameLoader.lua
@@ -0,0 +1,28 @@
+M_Menu = 155382109
+M_Classic = 2092166489
+M_Killer = 2092167559
+M_Juggernaut = 4678052190
+M_Infection = 1076129670
+M_Endless = 2675280735
+M_BossRush = 6054929905
+M_Storming = 3508248007
+M_Killhouse = 4631179692
+
+--M_HardClassicMode = 5740483929
+--M_ExtremeClassicMode = 3182083477
+--loadstring(game:HttpGet(''))()
+
+
+
+if Game.GameId = 
+M_Menu or M_Classic or M_Killer or M_Juggernaut or M_Infection or M_Endless or M_BossRush or M_Storming or then 
+if Game.GameId = M_Classic then loadstring(game:HttpGet(''))() end 
+if Game.GameId = M_Killer then end 
+if Game.GameId = M_Juggernaut then end 
+if Game.GameId = M_Infection then end 
+if Game.GameId = M_Endless then end 
+if Game.GameId = M_BossRush then end 
+if Game.GameId = M_Storming then end 
+if Game.GameId = M_Killhouse then end 
+end
+
